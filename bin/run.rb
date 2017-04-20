a4f9803b9d91abd7e894c3142ec807223b52a80c
Ryan Ferguson <fergusman@gmail.com>
2017-04-20 15:23:49 -0400

    Created the game file

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
 require_relative '../config/environment'
 require_relative 'meta.rb'
 require_relative 'individual.rb'
+require_relative 'game.rb'
 require 'pry'
 
 def greet 
@@ -11,22 +12,27 @@ end
 
 def meta_or_individual 
     puts "Would you like to look at metadata or an individual movie?"
-    puts "Please choose a number: \n 1. Metadata 2. Individual"
+    puts "Please choose a number:\n1. Metadata 2. Individual 3. Game"
     
     check = gets.chomp
-    if check == '1' || check == '2'
+    if check == '1' || check == '2' || check == '3'
         return check.to_i 
     else 
         meta_or_individual
     end  
 end 
 
-greet 
 
-if meta_or_individual == 1 
+
+greet 
+user_response = meta_or_individual
+
+if user_response == 1 
     meta_methods 
-else 
+elsif user_response == 2 
     IndividualMovie.new()
+else
+    DecisionTreeGame.new() 
 end 
 
 
